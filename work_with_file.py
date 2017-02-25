4b8bbd58565f675977ad13b407d884dd01046031
panyushkins <panyushkin@me.com>
2017-02-26 01:50:33 +0300

    Added read from JSON-serialized file

--- work_with_file.py
+++ work_with_file.py
@@ -4,3 +4,9 @@ import json
 def save_to_file(file_name, save_file):
     with open(file_name, 'w') as file:
         json.dump(save_file, file)
+
+
+def read_from_file(file_path):
+    with open(file_path, 'r') as file:
+        json_file = json.load(file)
+    return json_file
