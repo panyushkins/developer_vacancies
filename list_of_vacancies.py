bc527f0fd1368afabf29e9dfa86ddfa3819abead
panyushkins <panyushkin@me.com>
2017-02-26 20:26:13 +0300

    Added if __name__ == '__main__':

--- list_of_vacancies.py
+++ list_of_vacancies.py
@@ -1,15 +1,16 @@
 from work_with_api import make_superjob_request
 from work_with_file import save_to_file
 
-api_key = input('Please, enter your Superjob API key:')
-file_path = input('Please, input a path for file with results: ')
-dict_of_vacancies = {}
+if __name__ == '__main__':
+    api_key = input('Please, enter your Superjob API key:')
+    file_path = input('Please, input a path for file with results: ')
+    dict_of_vacancies = {}
 
-for times_of_requests in range(10):
-    parameters = {'keyword': 'разработчик', 'page': times_of_requests}
-    returned_vacancies = make_superjob_request(api_key, 'vacancies', parameters)
-    for vacancy in returned_vacancies['objects']:
-        dict_of_vacancies[vacancy['id_client']] = vacancy
+    for times_of_requests in range(10):
+        parameters = {'keyword': 'разработчик', 'page': times_of_requests}
+        returned_vacancies = make_superjob_request(api_key, 'vacancies', parameters)
+        for vacancy in returned_vacancies['objects']:
+            dict_of_vacancies[vacancy['id_client']] = vacancy
 
-save_to_file(file_path, dict_of_vacancies)
-print('File saved!')
+    save_to_file(file_path, dict_of_vacancies)
+    print('File saved!')
--- shorted_vacancies.py
+++ shorted_vacancies.py
@@ -1,17 +1,18 @@
 import work_with_file as helpers
 
-source_path = 'dict.txt'  # input('Please input path to a source file: ')
-final_path = 'dict2.txt'  # input('Please input path to a destination file: ')
-list_of_vacancies = helpers.read_from_file(source_path)
-shorted_vacancies = {}
+if __name__ == '__main__':
+    source_path = 'dict.txt'  # input('Please input path to a source file: ')
+    final_path = 'dict2.txt'  # input('Please input path to a destination file: ')
+    list_of_vacancies = helpers.read_from_file(source_path)
+    shorted_vacancies = {}
 
-for vacancy in list_of_vacancies:
-    shorted_vacancies[vacancy] = {'profession': list_of_vacancies[vacancy]['profession'],
-                                  'candidat': list_of_vacancies[vacancy]['candidat'],
-                                  'payment': list_of_vacancies[vacancy]['payment'],
-                                  'payment_from': list_of_vacancies[vacancy]['payment_from'],
-                                  'payment_to': list_of_vacancies[vacancy]['payment_to']
-                                  }
+    for vacancy in list_of_vacancies:
+        shorted_vacancies[vacancy] = {'profession': list_of_vacancies[vacancy]['profession'],
+                                      'candidat': list_of_vacancies[vacancy]['candidat'],
+                                      'payment': list_of_vacancies[vacancy]['payment'],
+                                      'payment_from': list_of_vacancies[vacancy]['payment_from'],
+                                      'payment_to': list_of_vacancies[vacancy]['payment_to']
+                                      }
 
-helpers.save_to_file(final_path, shorted_vacancies)
-print('Vacancies saved!')
+    helpers.save_to_file(final_path, shorted_vacancies)
+    print('Vacancies saved!')
