2e057cfc34bbf0db5e20260c21a75e70340878f2
panyushkins <panyushkin@me.com>
2017-02-26 21:52:06 +0300

    Changed the logic for 'payment' info

--- shorted_vacancies.py
+++ shorted_vacancies.py
@@ -1,18 +1,22 @@
 import work_with_file as helpers
 
 if __name__ == '__main__':
-    source_path = 'dict.txt'  # input('Please input path to a source file: ')
-    final_path = 'dict2.txt'  # input('Please input path to a destination file: ')
+    source_path = input('Please input path to a source file: ')
+    final_path = input('Please input path to a destination file: ')
     list_of_vacancies = helpers.read_from_file(source_path)
     shorted_vacancies = {}
 
     for vacancy in list_of_vacancies:
         shorted_vacancies[vacancy] = {'profession': list_of_vacancies[vacancy]['profession'],
-                                      'candidat': list_of_vacancies[vacancy]['candidat'],
-                                      'payment': list_of_vacancies[vacancy]['payment'],
-                                      'payment_from': list_of_vacancies[vacancy]['payment_from'],
-                                      'payment_to': list_of_vacancies[vacancy]['payment_to']
+                                      'candidat': list_of_vacancies[vacancy]['candidat']
                                       }
+
+        if list_of_vacancies[vacancy]['payment']:
+            shorted_vacancies[vacancy]['payment'] = list_of_vacancies[vacancy]['payment']
+        elif not list_of_vacancies[vacancy]['payment_from'] == 0 and not list_of_vacancies[vacancy]['payment_from'] == 0:
+            shorted_vacancies[vacancy]['payment'] = (list_of_vacancies[vacancy]['payment_from'] + list_of_vacancies[vacancy]['payment_from']) / 2
+        else:
+            shorted_vacancies[vacancy]['payment'] = list_of_vacancies[vacancy]['payment_from']
 
     helpers.save_to_file(final_path, shorted_vacancies)
     print('Vacancies saved!')
